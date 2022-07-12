da1fedf009f4ca74aed56339766f02cb8361c97b
Mai Mahmoud <mai.m.othman94@gmail.com>
2022-07-12 22:51:16 +0200

    My first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Worldw2");
+		System.out.println("Hello Worldwe");
+
 	}
 }
